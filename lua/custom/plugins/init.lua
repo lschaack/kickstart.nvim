d27398b36298f86c99ea11288b10a967a147d2fe
Luke Schaack <luke.schaack@gmail.com>
2026-02-24 12:02:15 -0800

    add visible context from treesitter

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -116,6 +116,21 @@ return {
     end,
   },
   {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'BufReadPost',
+    opts = {},
+    keys = {
+      {
+        '[c',
+        function()
+          require('treesitter-context').go_to_context(vim.v.count1)
+        end,
+        desc = 'Go to [c]ontext',
+        silent = true,
+      },
+    },
+  },
+  {
     dir = vim.fn.stdpath 'config' .. '/pack/plugins/start/github-url',
     name = 'github-url',
     lazy = false,
